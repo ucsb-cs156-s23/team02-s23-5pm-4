ab9e3a1fcdf4721b47c69d6581940ad8be849508
mike-k999 <michaelkuhn@Michaels-MacBook-Pro.local>
2023-05-09 15:14:40 -0700

    mk - Transport table set up in H2

--- src/main/java/edu/ucsb/cs156/example/repositories/TransportRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/TransportRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface TransportRepository extends CrudRepository<Transport, Long> {
+public interface TransportRepository extends CrudRepository<Transport, String> {
   
 }
