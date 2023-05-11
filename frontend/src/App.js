aa0223535a47856864215a795b418d1d7f08dd03
awal <ameymwalimbe@gmail.com>
2023-05-11 15:06:25 -0700

    amey - TreeController and all the other issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
   const { data: currentUser } = useCurrentUser();
 
   return (
-    <BrowserRouter basename="/team02-s23-5pm-4">
+    <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/profile" element={<ProfilePage />} />
